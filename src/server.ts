41d791206e9b5160207a49e883e11501318fe845
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-07 16:07:55 +0600

    refactor: . split && condition

--- src/server.ts
+++ src/server.ts
@@ -26,20 +26,25 @@ export function serverFunction(req: IncomingMessage, res: ServerResponse, data: 
                 res.writeHead(200, {'Content-Type': 'text/plain'});
                 res.end('OK');
             } else {
-                if (req.method === 'GET' && req.url && req.url.startsWith('/users/')) {
-                    const id = req.url.split('/')[2];
-                    if (data && data.users) {
-                        const user = data.users.find((u: any) => u.id == id);
-                        if (user) {
-                            res.writeHead(200, {'Content-Type': 'application/json'});
-                            res.end(JSON.stringify(user));
+                if (req.method === 'GET') {
+                    if (req.url && req.url.startsWith('/users/')) {
+                        const id = req.url.split('/')[2];
+                        if (data && data.users) {
+                            const user = data.users.find((u: any) => u.id == id);
+                            if (user) {
+                                res.writeHead(200, {'Content-Type': 'application/json'});
+                                res.end(JSON.stringify(user));
+                            } else {
+                                res.writeHead(404, {'Content-Type': 'text/plain'});
+                                res.end('User not found');
+                            }
                         } else {
-                            res.writeHead(404, {'Content-Type': 'text/plain'});
-                            res.end('User not found');
+                            res.writeHead(500, {'Content-Type': 'text/plain'});
+                            res.end('Internal Server Error');
                         }
                     } else {
-                        res.writeHead(500, {'Content-Type': 'text/plain'});
-                        res.end('Internal Server Error');
+                        res.writeHead(404, {'Content-Type': 'text/plain'});
+                        res.end('Not Found');
                     }
                 } else {
                     res.writeHead(404, {'Content-Type': 'text/plain'});
