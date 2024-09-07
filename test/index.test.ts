f92a5e5ef4b53adf596adce138f4829bcfd97901
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-07 17:22:39 +0600

    test: . move helpers to separate file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,34 +1,8 @@
-import * as Server from "../src/server";
-
-class ServerResponse {
-  public statusCode: number = 0;
-  public headers: any;
-  public message: string = '';
-
-  public writeHead(statusCode: number, headers: any) {
-    this.statusCode = statusCode;
-    this.headers = headers;
-  }
-
-  public end(message: string) {
-    this.message = message;
-  }
-}
-
-function testServerFunctionWithData(url: string, statusCode: number, contentType: string, message: any) {
-  const request = {
-    url,
-    method: "GET",
-  }
-
-  const response = new ServerResponse();
-  // @ts-ignore
-  Server.serverFunction(request, response)
-
-  expect(response.statusCode).toEqual(statusCode);
-  expect(response.headers).toEqual({"Content-Type": contentType})
-  expect(response.message).toEqual(message)
-}
+import {
+    testServerFunctionToReturn404Error,
+    testServerFunctionToReturn500Error,
+    testServerFunctionWithData
+} from "./testHelpers";
 
 describe('tests with regular data', () => {
   it.each([
@@ -77,21 +51,6 @@ describe('tests with regular data', () => {
   });
 })
 
-function testServerFunctionToReturn500Error(url: string, data: any) {
-    const request = {
-        url,
-        method: "GET",
-    }
-
-    const response = new ServerResponse();
-    // @ts-ignore
-    Server.serverFunction(request, response, data);
-
-    expect(response.statusCode).toEqual(500);
-    expect(response.headers).toEqual({"Content-Type": "text/plain"})
-    expect(response.message).toEqual('Internal Server Error');
-}
-
 describe('tests with missing data', () => {
     it.each([
         [
@@ -123,16 +82,6 @@ describe('tests with corrupt data', () => {
         testServerFunctionToReturn500Error(url, dataWithoutUsersField);
     });
 });
-
-function testServerFunctionToReturn404Error(badRequest: any) {
-    const response = new ServerResponse();
-    // @ts-ignore
-    Server.serverFunction(badRequest, response)
-
-    expect(response.statusCode).toEqual(404);
-    expect(response.headers).toEqual({"Content-Type": "text/plain"})
-    expect(response.message).toEqual('Not Found')
-}
 
 describe('wrong request tests', () => {
     it.each([
