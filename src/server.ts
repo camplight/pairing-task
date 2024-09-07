96b99240f1362afcbef5a4107cae99448fea067c
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-09-07 16:11:49 +0600

    refactor: - remove always false statement in this branch

--- src/server.ts
+++ src/server.ts
@@ -48,27 +48,7 @@ export function serverFunction(req: IncomingMessage, res: ServerResponse, data: 
             }
         }
     } else {
-        if (req.url === '/health' && req.method === 'GET') {
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('OK');
-        } else if (req.url && req.url.startsWith('/users/') && req.method === 'GET') {
-            const id = req.url.split('/')[2];
-            if (data && data.users) {
-                const user = data.users.find((u: any) => u.id == id);
-                if (user) {
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end(JSON.stringify(user));
-                } else {
-                    res.writeHead(404, {'Content-Type': 'text/plain'});
-                    res.end('User not found');
-                }
-            } else {
-                res.writeHead(500, {'Content-Type': 'text/plain'});
-                res.end('Internal Server Error');
-            }
-        } else {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not Found');
-        }
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
     }
 }
