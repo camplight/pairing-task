47c116dadfb0c0e73421d6d646ca22dcc3b04ce2
Vitaliy Filipov <altras@gmail.com>
2024-10-22 16:47:50 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/serverHandler.ts

--- src/serverHandler.ts
+++ src/serverHandler.ts
@@ -11,37 +11,56 @@ function getDataFromFile(fileName: string): any {
     }
 }
 
+// TODO
+// get rid of all elses
+
+function getUsersHandler(res: ServerResponse) {
+    const data = getDataFromFile('data.json');
+    if (data) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(data));
+    } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+    }
+}
+
 export function serverHandler(req: IncomingMessage, res: ServerResponse) {
     if (req.url === '/users' && req.method === 'GET') {
-        const data = getDataFromFile('data.json');
-        if (data) {
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(JSON.stringify(data));
-        } else {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
-        }
+        getUsersHandler(res);
     } else if (req.url === '/health' && req.method === 'GET') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('OK');
+        getHealthHandler(res);
     } else if (req.url && req.url.startsWith('/users/') && req.method === 'GET') {
-        const id = req.url.split('/')[2];
-        const data = getDataFromFile('data.json');
-        if (data && data.users) {
-            const user = data.users.find((u: any) => u.id == id);
-            if (user) {
-                res.writeHead(200, {'Content-Type': 'application/json'});
-                res.end(JSON.stringify(user));
-            } else {
-                res.writeHead(404, {'Content-Type': 'text/plain'});
-                res.end('User not found');
-            }
+        getUserByIdHandler(req, res);
+    } else {
+        NotFoundHandler(res);
+    }
+}
+
+function NotFoundHandler(res: ServerResponse<IncomingMessage>) {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+}
+
+function getUserByIdHandler(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const id = req.url.split('/')[2];
+    const data = getDataFromFile('data.json');
+    if (data && data.users) {
+        const user = data.users.find((u: any) => u.id == id);
+        if (user) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(user));
         } else {
-            res.writeHead(500, {'Content-Type': 'text/plain'});
-            res.end('Internal Server Error');
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('User not found');
         }
     } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not Found');
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
     }
+}
+
+function getHealthHandler(res: ServerResponse<IncomingMessage>) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('OK');
 }
