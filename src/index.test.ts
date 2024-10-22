a8044bc1e4da79513d1dbda4aa193c0a34322d68
idanmel <idanmel@gmail.com>
2024-10-22 15:35:49 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/index.test.ts

---
+++ src/index.test.ts
@@ -0,0 +1,4 @@
+// test GET /health -> should return 200, OK.
+// test GET /users with a json mock -> should return 200, and the json mock.
+// test GET /users without json mock -> should return 500
+
