df5f446161d4d14881172e13ea179ff8dc46494d
idanmel <idanmel@gmail.com>
2024-10-22 15:49:06 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,3 +7,7 @@
 // test GET /users/:id with a invalid json -> should return 500
 // test GET /whatever -> should return 404 with 'Not Found'
 // test POST /health -> should return 404mo
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+});
