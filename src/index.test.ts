ebbf0e323452ac23138cc9a06293338d668d632c
Vitaliy Filipov <altras@gmail.com>
2024-10-22 15:41:06 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/index.test.ts

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,9 @@
 // test GET /health -> should return 200, OK.
 // test GET /users with a json mock -> should return 200, and the json mock.
 // test GET /users without json mock -> should return 500
-
+// test GET /users with invalid json -> should return 500
+// test GET /users/:id with a json mock -> should return 200, and the whole user item
+// test GET /users/:id with a non-existing user id -> should return 404
+// test GET /users/:id with a invalid json -> should return 500
+// test GET /whatever -> should return 404 with 'Not Found'
+// test POST /health -> should return 404mo
